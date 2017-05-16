f2f0e02543b7fdbea5fc529cf7b705ade5d6f79a
surfertas <tasuku@gmail.com>
2017-05-16 15:59:24 +0900

    updated

--- turtlepi_navigate/src/target_generator.cpp
+++ turtlepi_navigate/src/target_generator.cpp
@@ -12,6 +12,7 @@ using costmap_2d::FREE_SPACE;
     {
         setParams();
         registerService();
+        registerPublisher();
         registerSubscriber();
     }
 
@@ -19,6 +20,14 @@ using costmap_2d::FREE_SPACE;
     {
         srv_generate_target_.shutdown();
     }
+
+    void TargetGenerator::setParams()
+    {
+        // set target orientation of robot.
+        theta_ = 90.0;
+        PI_ = 3.14159265358;
+    }
+
 
     void TargetGenerator::registerSubscriber()
     {
@@ -28,18 +37,16 @@ using costmap_2d::FREE_SPACE;
         sub_cost_map_update_ =
             nh_.subscribe( "/move_base/global_costmap/costmap_updates", 10, &TargetGenerator::costMapUpdateCB, this );
 
-        std::cout << "Registered subscriber." << std::endl;
-    }
+        sub_turtlepi_location_ = 
+            nh_.subscribe("/amcl_pose", 10, &TargetGenerator::currentPositionCB, this);
 
-    void TargetGenerator::setParams()
-    {
-        // set target orientation of robot.
-        theta_ = 90.0;
-        PI_ = 3.141592653589793238463;
+        std::cout << "Registered subscriber." << std::endl;
     }
 
     void TargetGenerator::registerPublisher()
     {
+        pub_visualization_marker_ = 
+            nh_.advertise<visualization_msgs::Marker>("/turtlepi_navigate/visualization_marker", 0);
     }
 
     void TargetGenerator::registerService()
@@ -65,6 +72,7 @@ using costmap_2d::FREE_SPACE;
         map_origin_y_ = grid_msg->info.origin.position.y;
 
         map_resolution_ = grid_msg->info.resolution;
+        map_data_ = grid_msg->data;
 
         std::cout << "Cost Map created!" << std::endl;
     }
@@ -82,19 +90,21 @@ using costmap_2d::FREE_SPACE;
 
     }
 
+    void TargetGenerator::currentPositionCB(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& location)
+    {
+        current_position_ = *location;
+    }
+
     bool TargetGenerator::generateTargetService(turtlepi_navigate::GenerateTarget::Request &req,
                                                  turtlepi_navigate::GenerateTarget::Response &res)
     {
-        std::cout << "CALLED SERV" << std::endl;
         std::random_device rd;  //Will be used to obtain a seed for the random number engine
         std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
         std::uniform_int_distribution<> grid_x(0, map_size_x_);
         std::uniform_int_distribution<> grid_y(0, map_size_y_);
 
-
         uint32_t map_x, map_y, idx;
         double world_x, world_y;
-        std::cout <<"Entering loop" << std::endl;
 
         do {
             map_x = grid_x(gen);
@@ -104,16 +114,22 @@ using costmap_2d::FREE_SPACE;
             mapToWorld(map_x, map_y, world_x, world_y);
 
             idx = map_x + map_y * map_size_x_;
-            std::cout << "idx: " << idx << std::endl;
-            std::cout << "idx: " << map_data_[idx] << std::endl;
+            std::cout << "idx: " << idx;
+            std::cout << "map_x:" << map_x;
+            std::cout << "map_y:" << map_y;
+            std::cout << "map_size_x_:" << map_size_x_;
+            std::cout << "current_x: " << current_position_.pose.pose.position.x;
+            std::cout << "current_y: " << current_position_.pose.pose.position.y;
+
+            std::cout << std::endl;
+            ROS_INFO("map data[idx]: %d\n", map_data_[idx]);
+
+        //TODO:Check /amcl_pose to get location of turtlebot, use threshold to see if
+        //ok to accept target.
 
         } while (map_data_[idx] != 0);
-        // TODO: Why doesnt this print here?????? NEED TO FOLLOW UP
-        std::cout <<"exit loop" << std::endl;
 
-        std::cout << "before radians\n";
         double radians = theta_ * (PI_/180.0);
-        std::cout << "radians: " << radians << std::endl;
         
         tf::Quaternion quaternion;
         quaternion = tf::createQuaternionFromYaw(radians);
@@ -128,7 +144,59 @@ using costmap_2d::FREE_SPACE;
         res.goal.target_pose.pose.orientation =  q_msg;
         res.success = true;
 
+        // TODO: move to separate function 
+        geometry_msgs::TransformStamped transformStamped;
+        try {
+            transformStamped = tfBuffer_.lookupTransform("map", "base_link",
+                                                         ros::Time(0));
+        } catch (tf2::TransformException &ex) {
+            ROS_WARN("%s", ex.what());
+            ros::Duration(1.0).sleep();
+        }
+
+
+        geometry_msgs::PoseStamped pose_in;
+        geometry_msgs::PoseStamped pose_out;
+
+        pose_in.pose.position.x = world_x;
+        pose_in.pose.position.y = world_y;
+        pose_in.header.stamp = ros::Time(0);
+        pose_in.header.frame_id = "base_link";
+
+        tfBuffer_.transform(pose_in, pose_out, "map");
+        // TODO: Need to figure correct transform. Should it be base_link to
+        // map? or something else.
+        // TODO: Need to create threshold (if target position is x distance away
+        // from current position.
+        targetMarker(pose_out.pose.position.x, pose_out.pose.position.y);
+
         return res.success;
+    }
+
+    void TargetGenerator::targetMarker(uint32_t x, uint32_t y)
+    {
+        visualization_msgs::Marker marker;
+        marker.header.frame_id = "map";
+        marker.header.stamp = ros::Time();
+        marker.ns = "turtlepi_navigate";
+        marker.id = 0;
+        marker.type = visualization_msgs::Marker::SPHERE;
+        marker.action = visualization_msgs::Marker::ADD;
+        marker.pose.position.x = (double)x;
+        marker.pose.position.y = (double)y;
+        marker.pose.position.z = 1;
+        marker.pose.orientation.x = 0.0;
+        marker.pose.orientation.y = 0.0;
+        marker.pose.orientation.z = 0.0;
+        marker.pose.orientation.w = 1.0;
+        marker.scale.x = 1;
+        marker.scale.y = 1;
+        marker.scale.z = 0.1;
+        marker.color.a = 1.0;
+        marker.color.r = 0.0;
+        marker.color.g = 1.0;
+        marker.color.b = 0.0;
+        pub_visualization_marker_.publish(marker);
     }
 
 }  // namespace turtlepi_navigate
