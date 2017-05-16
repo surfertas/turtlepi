12906997b9cda7acd3869105ad099c31a7ec1fa4
surfertas <tasuku@gmail.com>
2017-05-16 16:00:40 +0900

    updated

--- turtlepi_navigate/include/turtlepi_navigate/target_generator.h
+++ turtlepi_navigate/include/turtlepi_navigate/target_generator.h
@@ -7,9 +7,13 @@
 #include <random>
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <nav_msgs/OccupancyGrid.h>
 #include <map_msgs/OccupancyGridUpdate.h>
 #include <move_base_msgs/MoveBaseAction.h>
+#include <visualization_msgs/Marker.h>
 #include <costmap_2d/costmap_2d_ros.h>
 #include <costmap_2d/costmap_2d.h>    
 #include <costmap_2d/cost_values.h>
@@ -32,19 +36,25 @@ public:
     void registerService();
     void costMapUpdateCB(const map_msgs::OccupancyGridUpdate::ConstPtr& grid_msg);
     void costMapInitCB(const nav_msgs::OccupancyGrid::ConstPtr& grid_msg);
+    void currentPositionCB(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& location);
     bool generateTargetService(turtlepi_navigate::GenerateTarget::Request &req, 
                                turtlepi_navigate::GenerateTarget::Response &res);
-
+    
     void mapToWorld(uint32_t mx, uint32_t my, double& wx, double& wy);
-  
+    void targetMarker(uint32_t x, uint32_t y);
+
 private:
     ros::NodeHandle nh_;
+    ros::Publisher pub_visualization_marker_;
     ros::Subscriber sub_cost_map_;
     ros::Subscriber sub_cost_map_update_;
+    ros::Subscriber sub_turtlepi_location_;
     ros::ServiceServer srv_generate_target_;
-    tf::TransformListener tf_listener_;
+    tf2_ros::Buffer tfBuffer_;
+    tf2_ros::TransformListener listener_ {tfBuffer_}; 
     costmap_2d::Costmap2D cost_map_;
     std::vector<int8_t> map_data_;
+    geometry_msgs::PoseWithCovarianceStamped current_position_;
 
     uint32_t map_size_x_;
     uint32_t map_size_y_;
