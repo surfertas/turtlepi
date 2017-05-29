1f50b7e0a4c208b910342922e6d63572b53028ca
surfertas <tasuku@gmail.com>
2017-05-29 16:15:19 +0900

    updated

--- turtlepi_navigate/include/turtlepi_navigate/turtlepi_nav.h
+++ turtlepi_navigate/include/turtlepi_navigate/turtlepi_nav.h
@@ -5,7 +5,11 @@
 #define TURTLEPI_NAV_H
 
 #include <ros/ros.h>
+#include <rosbag/bag.h>
+#include <rosbag/stream.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <std_msgs/String.h>
+#include <std_srvs/SetBool.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
 #include <tf/transform_listener.h>
@@ -28,6 +32,8 @@ public:
 private:
   ros::NodeHandle nh_;
   MoveBaseClient ac_;
+
+  ros::Publisher pub_episode_result_;
 };
 }
 #endif  // TURTLEPI_NAV_H
