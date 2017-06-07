f15c04c8b605f0d3c1488aa89e2fcb90cab8c41d
surfertas <tasuku@gmail.com>
2017-06-07 17:26:35 +0900

    not building

--- turtlepi_recorder/include/turtlepi_recorder/turtlepi_recorder.h
+++ turtlepi_recorder/include/turtlepi_recorder/turtlepi_recorder.h
@@ -5,9 +5,17 @@
 #include <rosbag/recorder.h>
 //#include <turtlepi_recorder/RecordTopics.h>
 #include <std_srvs/SetBool.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <sensor_msgs/LaserScan.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
 
 namespace turtlepi_recorder
 {
+typedef message_filters::sync_policies::ApproximateTime<geometry_msgs::PoseWithCovarianceStamped, sensor_msgs::LaserScan> SyncPolicy;
+
 class TurtlepiRecorder
 {
 public:
@@ -15,12 +23,21 @@ public:
   virtual ~TurtlepiRecorder();
 
 private:
+  void registerSubscriber();
+  void registerPublisher();
   void registerService();
+  void timeSyncCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& pose_msg, 
+                        const sensor_msgs::LaserScan::ConstPtr& scan_msg);
   bool recorderControlService(std_srvs::SetBool::Request& req, std_srvs::SetBool::Response& res);
 
   ros::NodeHandle nh_;
   rosbag::Recorder recorder_;
+  ros::Publisher pub_sync_amcl_;
+  ros::Publisher pub_sync_scan_;
   ros::ServiceServer srv_recorder_control_;
+  message_filters::Subscriber<geometry_msgs::PoseWithCovarianceStamped> sub_amcl_pose_;
+  message_filters::Subscriber<sensor_msgs::LaserScan> sub_laser_scan_;
+  message_filters::Synchronizer<SyncPolicy> sync_;
 
   bool recorder_on_;
 };
